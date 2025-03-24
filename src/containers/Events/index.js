f9ac0fcd18d23bb89165febb05f3f1469fb19772
techernoelpierre <techernoelpierre@gmail.com>
2025-03-24 12:13:50 +0400

    fix: filters and pagination in Events component

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,23 +13,16 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+  const firstElement = (currentPage - 1) * PER_PAGE;
+  const filteredEvents = (data?.events || [])
+    .filter((el) => type ? el.type === type : true)
+    .slice(firstElement, firstElement + PER_PAGE);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
   return (

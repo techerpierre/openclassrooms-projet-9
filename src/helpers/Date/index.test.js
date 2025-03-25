3b8e0de48309d1279aad50e3c0a963e92020f0cb
techernoelpierre <techernoelpierre@gmail.com>
2025-03-25 11:20:30 +0400

    feat: implement tests for getMonth helper

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,14 +1,12 @@
-/**
- * 
- */
+import { getMonth } from ".";
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+            expect(getMonth(new Date("2022-01-01"))).toBe("janvier");
         });
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            expect(getMonth(new Date("2022-07-08"))).toBe("juillet");
         });
     });
 })
